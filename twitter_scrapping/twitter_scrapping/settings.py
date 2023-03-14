3bab0618be37940819f3758cc6cd1f94f6fb851a
Thiago Paim <thpaim@gmail.com>
2023-03-14 18:50:41 -0300

    adicionando configurações do celery

--- twitter_scrapping/twitter_scrapping/settings.py
+++ twitter_scrapping/twitter_scrapping/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+if DEBUG:
+    CELERY_TASK_ALWAYS_EAGER = True
