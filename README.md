69eee754b7487e1de4494d822663710f646d409e
Thiago Paim <thpaim@gmail.com>
2023-03-15 12:24:36 -0300

    Refinando logs do scrape_tweets

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # twitter-scrapping
 
 
-
-### Running with Django
+### Iniciando o Django
 
 ```
 python manage.py runserver
 ```
 
 
-### Running with Django and FastAPI
+### Iniciando um worker Celery local
 
 ```
-python -m uvicorn twitter_scrapping.asgi:app --reload
+celery -A twitter_scrapping worker -l INFO -f celery.log
+```
+
+
+### Iniciando uma task de scrapping
+
+```
+python manage.py shell
+from tweets.tasks import scrape_tweets
+username = 'ErikakHilton'
+since = '2023-03-01'
+until = '2023-03-15'
+scrape_tweets.delay(username, since, until)
 ```
--- twitter_scrapping/tweets/tasks.py
+++ twitter_scrapping/tweets/tasks.py
@@ -6,29 +6,7 @@ from .models import Tweet, TwitterUser
 
 logger = get_task_logger(__name__)
 
-"""
-from tweets.tasks import test_log
-test_log.delay('test_log task')
-"""
-@shared_task
-def test_log(msg):
-    logger.info(msg)
-    try:
-        raise Exception('forced exception 1')
-    except Exception as e:
-        tb = traceback.format_exc()
-        logger.error(f'Exception({e}):\n{tb}')
-    raise Exception('forced exception 2')
-    return msg
 
-
-"""Exemplo de execução pelo django shell
-from tweets.tasks import scrape_tweets
-username = 'carteiroreaca'
-since = '2023-03-01'
-until = '2023-03-15'
-scrape_tweets.delay(username, since, until)
-"""
 @shared_task
 def scrape_tweets(username, since, until, recurse=False):
     logger.info(f'Iniciando scrape_tweets(username={username}, since={since}, until={until}, recurse={recurse})')
@@ -40,13 +18,13 @@ def scrape_tweets(username, since, until, recurse=False):
     query = f'from:{username} since:{since} until:{until}'
     user_scrapping_results = sntwitter.TwitterSearchScraper(query)
 
-    logger.info(f'Lendo tweets do usuário {username}')
+    logger.info(f'Iterando tweets do usuário "{username}"')
     tweet_ids = []
     for tweet in user_scrapping_results.get_items():
         tweet_ids.append(tweet.id)
     logger.info(f'Encontrados {len(tweet_ids)} tweets')
 
-    logger.info('Raspando tweets originais e respostas')
+    logger.info(f'Raspando tweets do usuário "{username}" e suas respostas')
     tweets_and_replies = []
     for tweet_id in tweet_ids:
         tweet_scrapper = sntwitter.TwitterTweetScraper(tweet_id, mode=mode)
@@ -77,26 +55,33 @@ def scrape_tweets(username, since, until, recurse=False):
     logger.info(f'Iniciando gravacao de {len(tweets_and_replies)} tweets')
     new_tweets = []
     for tweet_data in tweets_and_replies:
+        logger.info(f'Salvando {tweet_data}')
         try:
             user_kwargs = {
                 model_key: getattr(tweet_data.user, user_key) for user_key, model_key in user_key_mapping.items()
             }
-            u = TwitterUser.objects.get(twitter_id=user_kwargs['twitter_id'])
-            # Não está atualizando o usuario caso alguma informação tenha mudado
-        except TwitterUser.DoesNotExist:
-            u = TwitterUser.objects.create(**user_kwargs)
+            try:
+                # Usuário não é atualizado caso tenha alguma alteração
+                u = TwitterUser.objects.get(twitter_id=user_kwargs['twitter_id'])
+            except TwitterUser.DoesNotExist:
+                u = TwitterUser.objects.create(**user_kwargs)
+            
+            tweet_kwargs = {model_key: getattr(tweet_data, tweet_key) for tweet_key, model_key in tweet_key_mapping.items()}
+            tweet_kwargs['user'] = u
+            try:
+                # Tweet não é atualizado caso tenha alguma alteração
+                tweet_id = tweet_kwargs['twitter_id']
+                t = Tweet.objects.get(twitter_id=tweet_id)
+                logger.info(f'Tweet {tweet_id} já existe na base')
+            except Tweet.DoesNotExist:
+                t = Tweet.objects.create(**tweet_kwargs)
+                logger.info(f'Tweet {tweet_id} criado')
+                new_tweets.append(t)
+                
         except AttributeError as e:
-            # To Do: Investigar esse erro
-            # Em alguns casos ocorre um erro de que um objeto ´Tombstone´ não possui ´user´
+            tb = traceback.format_exc()
+            logger.error(f'Erro ao salvar tweet {tweet_data}')
+            logger.error(f'Exception({e}):\n{tb}')
             continue
-        
-        kwargs = {model_key: getattr(tweet_data, tweet_key) for tweet_key, model_key in tweet_key_mapping.items()}
-        kwargs['user'] = u
-        
-        try:
-            t = Tweet.objects.get(twitter_id=kwargs['twitter_id'])
-        except Tweet.DoesNotExist:
-            t = Tweet.objects.create(**kwargs)
-            new_tweets.append(t)
-        
-    logger.info(f'Finalizando tarefa scrape_tweets(username={username}, since={since}, until={until}, recurse={recurse}): {len(new_tweets)} novos tweets salvos')
+
+    logger.info(f'Finalizando scrape_tweets(username={username}, since={since}, until={until}, recurse={recurse}): {len(new_tweets)} novos tweets salvos')
