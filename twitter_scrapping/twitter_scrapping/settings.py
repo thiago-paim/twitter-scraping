fdda65d4defee0df56d2643b0eebd7d28b9d5936
Thiago Paim <thpaim@gmail.com>
2023-03-27 12:51:40 -0300

    working version; still needs cleanup

--- twitter_scrapping/twitter_scrapping/settings.py
+++ twitter_scrapping/twitter_scrapping/settings.py
@@ -10,25 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env(Path(str(BASE_DIR)).joinpath('.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = env("DEBUG")
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -129,4 +132,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_EXPORT_PATH = f'{BASE_DIR}/exports/'
 
-CELERY_BROKER_URL=os.environ.get("CELERY_BROKER_URL")
+CELERY_BROKER_URL="amqp://guest:guest@rabbitmq:5672//"
