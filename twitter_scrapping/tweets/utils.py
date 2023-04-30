2377427d50e96ffc3c1b680e7327bc98ebd3f61a
Thiago Paim <thpaim@gmail.com>
2023-04-30 02:30:11 -0300

    add CustomTwitterTweetScraper

--- twitter_scrapping/tweets/utils.py
+++ twitter_scrapping/tweets/utils.py
@@ -2,13 +2,18 @@ from copy import deepcopy
 from django.conf import settings
 from django.utils import timezone
 import json
+import logging
 import pandas as pd
+from snscrape.base import ScraperException
 from snscrape.modules.twitter import (
     TwitterProfileScraper,
+    TwitterTweetScraper,
     TweetRef,
     User as SNUser,
     Tweet as SNTweet,
 )
+
+_logger = logging.getLogger(__name__)
 
 
 def export_csv(queryset, filename=None):
@@ -28,6 +33,91 @@ class CustomTwitterProfileScraper(TwitterProfileScraper):
             if not "rest_id" in result["core"]["user_results"]["result"]:
                 return TweetRef(id=result["rest_id"])
         return super()._graphql_timeline_tweet_item_result_to_tweet(result, tweetId)
+
+
+class CustomTwitterTweetScraper(TwitterTweetScraper):
+    # Workaround for dealing with issue when scraping tweet 1651646573225418756
+    def _graphql_timeline_instructions_to_tweets(
+        self, instructions, includeConversationThreads=False
+    ):
+        for instruction in instructions:
+            if instruction["type"] != "TimelineAddEntries":
+                continue
+            for entry in instruction["entries"]:
+                if entry["entryId"].startswith("tweet-"):
+                    tweetId = int(entry["entryId"].split("-", 1)[1])
+                    if (
+                        entry["content"]["entryType"] == "TimelineTimelineItem"
+                        and entry["content"]["itemContent"]["itemType"]
+                        == "TimelineTweet"
+                    ):
+                        if (
+                            "result"
+                            not in entry["content"]["itemContent"]["tweet_results"]
+                        ):
+                            _logger.warning(
+                                f'Skipping empty tweet entry {entry["entryId"]}'
+                            )
+                            continue
+                        yield self._graphql_timeline_tweet_item_result_to_tweet(
+                            entry["content"]["itemContent"]["tweet_results"]["result"],
+                            tweetId=tweetId,
+                        )
+                    else:
+                        _logger.warning("Got unrecognised timeline tweet item(s)")
+                elif entry["entryId"].startswith("homeConversation-"):
+                    if entry["content"]["entryType"] == "TimelineTimelineModule":
+                        for item in entry["content"]["items"]:
+                            if (
+                                not item["entryId"].startswith("homeConversation-")
+                                or "-tweet-" not in item["entryId"]
+                            ):
+                                raise ScraperException(
+                                    f'Unexpected home conversation entry ID: {item["entryId"]!r}'
+                                )
+                            tweetId = int(item["entryId"].split("-tweet-", 1)[1])
+                            if (
+                                item["item"]["itemContent"]["itemType"]
+                                == "TimelineTweet"
+                            ):
+                                if (
+                                    "result"
+                                    in item["item"]["itemContent"]["tweet_results"]
+                                ):
+                                    yield self._graphql_timeline_tweet_item_result_to_tweet(
+                                        item["item"]["itemContent"]["tweet_results"][
+                                            "result"
+                                        ],
+                                        tweetId=tweetId,
+                                    )
+                                else:
+                                    yield TweetRef(id=tweetId)
+                elif includeConversationThreads and entry["entryId"].startswith(
+                    "conversationthread-"
+                ):  # TODO show more cursor?
+                    for item in entry["content"]["items"]:
+                        if item["entryId"].startswith(f'{entry["entryId"]}-tweet-'):
+                            if (
+                                len(
+                                    item["entryId"][len(entry["entryId"]) + 7 :].split(
+                                        "-"
+                                    )
+                                )
+                                > 1
+                            ):
+                                _logger.warning(
+                                    f'Skipping unrecognised entry ID: {entry["entryId"]!r}'
+                                )
+                                continue
+                            tweetId = int(item["entryId"][len(entry["entryId"]) + 7 :])
+                            yield self._graphql_timeline_tweet_item_result_to_tweet(
+                                item["item"]["itemContent"]["tweet_results"]["result"],
+                                tweetId=tweetId,
+                            )
+                elif not entry["entryId"].startswith("cursor-"):
+                    _logger.warning(
+                        f'Skipping unrecognised entry ID: {entry["entryId"]!r}'
+                    )
 
 
 def tweet_to_json(tweet):
