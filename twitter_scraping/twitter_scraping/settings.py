b2453ef2450e0e72ae2f573429887917dfac8410
Thiago Paim <thpaim@gmail.com>
2023-06-13 10:56:12 -0300

    updating settings

--- twitter_scraping/twitter_scraping/settings.py
+++ twitter_scraping/twitter_scraping/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT =  f"{BASE_DIR}/static/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -143,6 +146,8 @@ CELERY_BEAT_SCHEDULE = {
     #     "schedule": crontab(minute="*/30"),
     # },
 }
+
+FLOWER_PORT = env("FLOWER_PORT")
 
 
 # Scraping Settings
